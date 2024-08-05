f4e16d8e1c2e4bb2bea5c984aeaac04c46ff1fcd
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-08-05 23:11:37 +0300

    fix unnecessary repetition of jsx, remove user data

--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import User from "./User";
 import "../App.css";
 
 function Tasks() {
@@ -7,15 +9,13 @@ function Tasks() {
   const [content, setContent] = useState("");
   const [tasks, setTasks] = useState([]);
   const [selectedTask, setSelectedTask] = useState(null);
-  const [users, setUsers] = useState([]);
-  const [selectedRoles, setSelectedRoles] = useState({});
+  const navigate = useNavigate();
   const role = localStorage.getItem("role");
-  const refreshToken = localStorage.getItem("refreshToken");
   useEffect(() => {
-    const fetchToken = async () => {
+    const fetchToken = () => {
       try {
-        await axios.post("http://localhost:5000/refresh-token", {
-          refreshToken: refreshToken,
+        axios.post("http://localhost:5000/refresh-token", null, {
+          withCredentials: true,
         });
       } catch (error) {
         console.error(
@@ -30,22 +30,18 @@ function Tasks() {
   useEffect(() => {
     const fetchTasks = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/tasks");
-        const initialRoles = response.data.users.reduce((acc, user) => {
-          acc[user.id] = user.role;
-          return acc;
-        }, {});
-        setSelectedRoles(initialRoles);
-        setTasks(response.data.tasks);
-        setUsers(response.data.users);
+        const response = await axios.get("http://localhost:5000/tasks", {
+          withCredentials: true,
+        });
+        setTasks(response.data);
       } catch (error) {
+        navigate("/login");
         console.error(
           "Error fetching tasks:",
           error.response ? error.response.data : error.message
         );
       }
     };
-
     fetchTasks();
   }, []);
   const handleTasks = async (e) => {
@@ -94,177 +90,65 @@ function Tasks() {
     setContent(task.content);
     setSelectedTask(task);
   };
-
-  const handleRoleChange = async (userId) => {
-    const newRole = selectedRoles[userId];
-    try {
-      await axios.put(`http://localhost:5000/tasks/users/${userId}`, {
-        id: userId,
-        role: newRole,
-      });
-      setUsers((prevUsers) =>
-        prevUsers.map((user) =>
-          user.id === userId ? { ...user, role: newRole } : user
-        )
-      );
-    } catch (error) {
-      console.error(
-        "Error updating role:",
-        error.response ? error.response.data : error.message
-      );
-    }
-  };
-  const handleRoleSelectChange = (userId, newRole) => {
-    setSelectedRoles((prevSelectedRoles) => ({
-      ...prevSelectedRoles,
-      [userId]: newRole,
-    }));
-  };
+  const canViewTasks =
+    role === "VIEWER" ||
+    role === "ADMIN" ||
+    role === "EDITOR" ||
+    role === "SUPERADMIN";
+  const canEditTasks =
+    role === "ADMIN" || role === "EDITOR" || role === "SUPERADMIN";
+  const canDeleteTasks = role === "ADMIN" || role === "SUPERADMIN";
   return (
     <>
-      {role === "VIEWER" && (
+      {role === "SUPERADMIN" && <User />}
+
+      {canViewTasks && (
         <div>
-          {tasks &&
-            tasks.map((task) => (
-              <div key={task.id}>
-                <p>{task.title}</p>
-                <p>{task.content}</p>
+          {canEditTasks && (
+            <form onSubmit={handleTasks}>
+              <div>
+                <label>Title:</label>
+                <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
               </div>
-            ))}
-        </div>
-      )}
-      {role === "EDITOR" && (
-        <div>
+              <div>
+                <label>Content:</label>
+                <input
+                  type="text"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                />
+              </div>
+              <button type="submit">
+                {selectedTask ? "Update Task" : "Create Task"}
+              </button>
+            </form>
+          )}
+
           <div>
-            {tasks &&
+            {tasks && tasks.length > 0 ? (
               tasks.map((task) => (
                 <div key={task.id}>
                   <p>{task.title}</p>
                   <p>{task.content}</p>
-                  <button onClick={() => handleSelectTask(task)}>UPDATE</button>
+                  {canEditTasks && (
+                    <button onClick={() => handleSelectTask(task)}>
+                      UPDATE
+                    </button>
+                  )}
+                  {canDeleteTasks && (
+                    <button onClick={() => handleDelete(task.id)}>
+                      DELETE
+                    </button>
+                  )}
                 </div>
-              ))}
-          </div>
-          <form onSubmit={handleTasks}>
-            <div>
-              <label>Title:</label>
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </div>
-            <div>
-              <label>Content:</label>
-              <input
-                type="text"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
-            </div>
-            <button type="submit">
-              {selectedTask ? "Update Task" : "Create Task"}
-            </button>
-          </form>
-        </div>
-      )}
-      {role === "ADMIN" && (
-        <div>
-          <div>
-            {tasks &&
-              tasks.map((task) => (
-                <div key={task.id}>
-                  <p>{task.title}</p>
-                  <p>{task.content}</p>
-                  <button onClick={() => handleDelete(task.id)}>DELETE</button>
-                  <button onClick={() => handleSelectTask(task)}>UPDATE</button>
-                </div>
-              ))}
-          </div>
-
-          <form onSubmit={handleTasks}>
-            <div>
-              <label>Title:</label>
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </div>
-            <div>
-              <label>Content:</label>
-              <input
-                type="text"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
-            </div>
-            <button type="submit">
-              {selectedTask ? "Update Task" : "Create Task"}
-            </button>
-          </form>
-        </div>
-      )}
-      {role === "SUPERADMIN" && (
-        <div>
-          <div>
-            {tasks &&
-              tasks.map((task) => (
-                <div key={task.id}>
-                  <p>{task.title}</p>
-                  <p>{task.content}</p>
-                  <button onClick={() => handleDelete(task.id)}>DELETE</button>
-                  <button onClick={() => handleSelectTask(task)}>UPDATE</button>
-                </div>
-              ))}
-          </div>
-
-          <form onSubmit={handleTasks}>
-            <div>
-              <label>Title:</label>
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </div>
-            <div>
-              <label>Content:</label>
-              <input
-                type="text"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
-            </div>
-            <button type="submit">
-              {selectedTask ? "Update Task" : "Create Task"}
-            </button>
-          </form>
-
-          <div>
-            {users &&
-              users.map((user) => (
-                <div key={user.id}>
-                  <p>{user.email}</p>
-                  <p>{user.role}</p>
-                  <p>{user.accessToken}</p>
-                  <select
-                    name="role"
-                    value={selectedRoles[user.id] || user.role}
-                    onChange={(e) =>
-                      handleRoleSelectChange(user.id, e.target.value)
-                    }
-                  >
-                    <option value="VIEWER">VIEWER</option>
-                    <option value="EDITOR">EDITOR</option>
-                    <option value="ADMIN">ADMIN</option>
-                    <option value="SUPERADMIN">SUPERADMIN</option>
-                  </select>
-                  <button onClick={() => handleRoleChange(user.id)}>
-                    Change Role
-                  </button>
-                </div>
-              ))}
+              ))
+            ) : (
+              <p>No tasks available</p>
+            )}
           </div>
         </div>
       )}
